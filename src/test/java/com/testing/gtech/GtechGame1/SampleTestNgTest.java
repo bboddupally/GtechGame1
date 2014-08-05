38ddd0b062685f3f950ca719eb8dc9886e961196
bboddupally <b.bhupeshsharma@gmail.com>
2014-08-05 11:38:24 +0530

    this is a lab test

---
+++ src/test/java/com/testing/gtech/GtechGame1/SampleTestNgTest.java
@@ -0,0 +1,25 @@
+package com.testing.gtech.GtechGame1;
+
+import org.openqa.selenium.support.PageFactory;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.testing.gtech.GtechGame1.pages.HomePage;
+
+public class SampleTestNgTest extends TestNgTestBase {
+
+  private HomePage homepage;
+
+  @BeforeMethod
+  public void initPageObjects() {
+    homepage = PageFactory.initElements(driver, HomePage.class);
+  }
+
+  @Test
+  public void testHomePageHasAHeader() {
+    driver.get(baseUrl);
+    Assert.assertFalse("".equals(homepage.header.getText()));
+  }
+}
